030fbde2e5e3ad083034716f4adb3223104a8ca2
hmc-cs-jmwang <jmwang@cs.hmc.edu>
2016-11-12 15:15:35 -0800

    Trouble adding Facebook sharing. Error Message: socialsharing undefined

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -88,16 +88,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       'tab-chats': {
         templateUrl: 'templates/tab-chats.html',
         controller: 'ChatsCtrl'
-      }
-    }
-  })
-
-  .state('tab.chat-detail', {
-    url: '/chats/:chatId',
-    views: {
-      'tab-chats': {
-        templateUrl: 'templates/chat-detail.html',
-        controller: 'ChatDetailCtrl'
       }
     }
   })
