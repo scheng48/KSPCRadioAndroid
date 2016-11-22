7d8a87df073317fa1f87c6609dfb4326b331250d
Sally Cheng <sally.cheng48@gmail.com>
2016-11-21 19:01:21 -0800

    created directory page with list of days

--- www/js/app.js
+++ www/js/app.js
@@ -83,14 +83,23 @@ angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter
     })
 
   .state('tab.chats', {
-    url: '/chats',
-    views: {
-      'tab-chats': {
-        templateUrl: 'templates/tab-chats.html',
-        controller: 'ChatsCtrl'
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
       }
-    }
-  })
+    })
+  .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
+    })
 
   .state('tab.directory ', {
     url: '/directory',
